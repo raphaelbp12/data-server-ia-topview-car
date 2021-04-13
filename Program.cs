c87338f3ce78cba6f454cb16b83a49dc94dea641
Raphael Parreira <raphaelbp12@gmail.com>
2021-04-13 17:15:44 +0200

    project init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_core_flicker_project_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
